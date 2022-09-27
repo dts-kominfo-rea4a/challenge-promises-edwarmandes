e5c36c8187aaba4b8f572c35c831953b694ccadc
edwarmandes <edwar.armandes@gmail.com>
2022-09-27 19:50:03 +0700

    coba commit 1

--- promise.js
+++ promise.js
@@ -1,8 +1,48 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let bioskopMarah = 0;
+let bioskopTidakMarah=0;
+const promiseOutput = emosi => {      
+  if (emosi === 'marah') {
+    return promiseTheaterIXX().then((value) =>{
+      value.forEach((array) => {
+        if (array.hasil === 'marah') {
+          bioskopMarah++;
+        }
+        else if (array.hasil === 'tidak marah') {
+          bioskopTidakMarah++;  
+        }
+        else {
+          'error';
+        }
+      });
+      console.log ('Hasil Bioskop marah :')
+      return bioskopMarah
+            
+    });
+    
+  }    
+  if (emosi === 'tidak marah') {
+    return promiseTheaterIXX().then((value) =>{
+      value.forEach((array) => {
+        if (array.hasil === 'marah') {
+          bioskopMarah++;
+        }
+        else if (array.hasil === 'tidak marah') {
+          bioskopTidakMarah++;  
+        }
+        else {
+          'error';
+        }
+      });
+      console.log('Hasil Bioskop Tidak Marah :')
+      return bioskopMarah
 
+    });
+    
+  }    
+}
 module.exports = {
   promiseOutput,
 };
